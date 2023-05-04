7ed5408bfbc9a72938f698394988d809e6d747ea
VIVEK MULUKANOORI <109861968+vivekmulukanoori@users.noreply.github.com>
2023-05-04 18:51:12 +0530

    Update indexnew.js

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ var http = require('http');
 
 //create a server object:
 http.createServer(function (req, res) {
-  res.write('** this is my version 4.0 :) **'); //write a response to the client
+  res.write('** this is my version 5.0 :) **'); //write a response to the client
   res.end(); //end the response
 }).listen(80); //the server object listens on port 80
